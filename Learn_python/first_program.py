3c3abf6c95d509dbaba06d313ed29f4b4675c796
Suman Devkota <37627609+sumandevkota@users.noreply.github.com>
2020-03-22 16:46:23 -0400

    Add files via upload

---
+++ Learn_python/first_program.py
@@ -0,0 +1,13 @@
+
+# date: Mar 22, 2020
+# Program: Add two numbers
+print('\n'*10)
+print ("                                                     we will do the addition of two numbers                      ")
+
+a = float (input (" entre first number: "))
+b = float (input (" entre second number: "))
+c =  a + b
+
+
+
+print(" The sum is: ",c)
