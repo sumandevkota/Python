4ae103d48c92c4ab4a1948eca688718047a91f21
Suman Devkota <37627609+sumandevkota@users.noreply.github.com>
2021-11-24 09:37:58 -0500

    Add files via upload

--- files_start.py
+++ files_start.py
@@ -5,21 +5,30 @@
 def main():  
   # Open a file for writing and create it if it doesn't exist
 
-  f = open("textfile.txt","w+")
+  #f = open("textfile.txt","w+") # to create file
+  #f = open("textfile.txt","a")   # open file in append mode
+  f = open("textfile.txt","r")  # open file in read mode
 
 
   # Open the file for appending text to the end
   
      
   # write some lines of data to the file
-  for i in range(10):
-    f.write("This is line " + str(i) + "\r\n")
+  #for i in range(10):
+   # f.write("This is line " + str(i) + "\r\n")
+    #f.write("Suman_Devkota\n")
+
   
   # close the file when done
-  f.close()
+  #f.close()
   
   # Open the file back up and read the contents
-
+  if f.mode == 'r':
+   # contents = f.read() to read all file
+   fl = f.readline()
+   for x in fl:
+    print(x)
+  #print(contents)  
     
 if __name__ == "__main__":
   main()
