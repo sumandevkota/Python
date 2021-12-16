4cfd816a18a6ab6f5de6fe1a05570f92e547f448
Suman Devkota <37627609+sumandevkota@users.noreply.github.com>
2021-12-16 17:13:49 -0500

    Add files via upload

---
+++ Data_Manipulations.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+
+
+
+#data = pd.read_csv("data.csv")
+#print(data) # printing to observe whats data sets looks like
+
+#print(data.head(5)) # first 5 rows
+#print(data.columns) # gives all the columns in the data sets
+
+#Age_fare = data[['Age','Fare']] # just taking age and fare column from big data set
+# line below is commented because we have already created the file
+#Age_fare.to_csv("Age_fare.csv") # exporting a csv file with just age and fare column
+
+#lin = pd.read_csv("Age_fare.csv") # reading newly created file
+#print(lin)
+#print(lin.dtypes)  # printing data types of each column
+
+# changing age to integer from float
+#lin['Age'] = lin['Age'].astype(int)
+#print(lin.dtypes)
+#updating the data dype in the data set by exporting it
+#lin.to_csv("Age_fare.csv")
+
+#data = pd.read_csv("Age_fare.csv")
+#print(data.head())
+
+#data = data.drop(['Unnamed: 0','Unnamed: 0.1'], axis = 1 )
+#print(data.head())
+#data.to_csv("New.csv") # Creating new file with just age and fare
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
